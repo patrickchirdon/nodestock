aa5bab6c01f9a617c56d5741ead5316f967ce01c
Patrick Chirdon <pc419714@ohio.edu>
2022-06-14 04:13:45 -0400

    toggle added

--- index.js
+++ index.js
@@ -4,25 +4,57 @@ const app=express()
 const exphbs =require('express-handlebars');
 
 const path = require('path');
+const request = require('request')
+const bodyParser= require('body-parser');
 
 const PORT=process.env.PORT || 5000;
 
+//use body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
 
 
-const otherstuff = "hello there, this is otherstuff"
-
-
-
-
+function call_api(finishedAPI, ticker){
+	request('https://cloud.iexapis.com/stable/stock/' + ticker +'/quote?token=pk_2711a2706e924888a2a063e6e4cf4307', {json: true}, (err, res,body)=> {
+	if(err){ return console.log(err);}
+	if(res.statusCode ===200){
+		finishedAPI(body);
+		}
+	});
+};
 
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
+
+
+//Set index handlebar index  GET routes
 app.get('/', function(req, res){
-	res.render('home', {
-		stuff: otherstuff
-});
+		call_api(function(doneAPI){
+            
+			res.render('home', {
+			stock: doneAPI
+		
+		});
+	});
 
 });
+
+//Set index handlebar index  POST routes
+app.post('/', function(req, res){
+		call_api(function(doneAPI){
+			//posted_stuff =req.body.stock_ticker;
+			res.render('home', {
+			stock: doneAPI,
+			
+			
+				
+		
+		});
+	}, req.body.stock_ticker);
+
+});
+
+
+
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
